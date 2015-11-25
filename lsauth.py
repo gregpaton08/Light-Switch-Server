a581bc7e53826320cabb0271ebd86d13a5c11fed
gregpaton08 <gregpaton08@gmail.com>
2015-11-25 03:54:32 +0000

    [mod] Updated add user in lsauth.

--- lsauth.py
+++ lsauth.py
@@ -4,22 +4,28 @@ import argparse
 import pickle
 import md5
 import os
+import getpass
 
 
 LS_AUTH_DB_FILE = 'lsauth_users.p'
 
 
-def add_user(username, password):
-    if len(username) > 100 or len(password) > 100:
-        print('ERROR: username or password exceeds character limit (100)')
-        quit()
-
+def add_user(username):
     auth_users = {}
     if os.path.isfile(LS_AUTH_DB_FILE):
         auth_users = pickle.load(open(LS_AUTH_DB_FILE, "rb"))
 
     if username in auth_users.keys():
-        print('ERROR: {0} already in database'.format(username))
+        print('ERROR: \'{0}\' already in database'.format(username))
+        quit()
+
+    password = getpass.getpass()
+    if password != getpass.getpass():
+        print('ERROR: passwords do not match!')
+        quit()
+
+    if len(username) > 100 or len(password) > 100:
+        print('ERROR: username or password exceeds character limit (100)')
         quit()
 
     m = md5.new()
@@ -27,10 +33,12 @@ def add_user(username, password):
 
     auth_users[username] = m.digest()
 
+    pickle.dump(auth_users, open(LS_AUTH_DB_FILE, "wb"))
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Module for authenticating users')
-    parser.add_argument('-add', nargs=2, metavar=('user', 'password'), help='add user')
+    parser.add_argument('-add', nargs=1, metavar='user', help='add user')
     parser.add_argument('-rem', nargs=1, metavar='user', help='remove user')
 
     return parser.parse_args()
@@ -40,6 +48,6 @@ if __name__ == '__main__':
     args = get_args()
 
     if args.add:
-        add_user(args.add[0], args.add[1])
+        add_user(args.add[0])
     elif args.rem:
         pass
