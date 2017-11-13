80f8c43e42bb3e7165bf33ab2fa23e6f6200777d
gregpaton08 <gregpaton08@gmail.com>
2017-11-13 16:20:05 -0500

    Remove unused code

--- switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
+++ switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
@@ -1,51 +1,20 @@
-/*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- */
-
-/**
- * Example using Dynamic Payloads 
- *
- * This is an example of how to use payloads of a varying (dynamic) size. 
- */
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
-
-//
-// Hardware configuration
-//
 
 // Set up nRF24L01 radio on SPI bus plus pins 7 & 8
 
 RF24 radio(9, 10);
 
-// sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
-// Leave open to be the 'ping' transmitter
-const int role_pin = 5;
-
-//
-// Topology
-//
-
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t writingPipe = 0xF0F0F0F0E1LL;
 const uint64_t readingPipe = 0xF0F0F0F0D2LL;
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 //
 // Payload
 //
 
-const int min_payload_size = 4;
 const int max_payload_size = 32;
-const int payload_size_increments_by = 1;
-int next_payload_size = min_payload_size;
-
 char receive_payload[max_payload_size + 1]; // +1 to allow room for a terminating NULL char
 
 void setup(void)
@@ -94,7 +63,7 @@ void loop(void)
     radio.stopListening();
 
     // Send the final one back.
-    radio.write( receive_payload, len );
+    radio.write(receive_payload, len);
     Serial.println(F("Sent response."));
 
     // Now, resume listening so we catch the next packets.
