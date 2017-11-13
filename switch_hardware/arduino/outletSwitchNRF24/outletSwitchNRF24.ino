43b184eb1b7b275ea58b94862583ceaf4c061344
gregpaton08 <gregpaton08@gmail.com>
2017-11-13 15:10:14 -0500

    NRF24 proof of concept

--- switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
+++ switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
@@ -1,9 +1,91 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
 
+// Set up nRF24L01 radio on SPI bus plus pins 9 and 10.
+RF24 radio(9, 10);
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+void setup() {
+
+  Serial.begin(9600);
+
+  radio.begin();
+
+  // enable dynamic payloads
+  radio.enableDynamicPayloads();
+
+  // optionally, increase the delay between retries & # of retries
+  radio.setRetries(5,15);
+
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  radio.startListening();
+  radio.printDetails();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+const int min_payload_size = 4;
+const int max_payload_size = 32;
+const int payload_size_increments_by = 1;
+int next_payload_size = min_payload_size;
+char receive_payload[max_payload_size+1];
 
+void loop() {
+  // The payload will always be the same, what will change is how much of it we send.
+  static char send_payload[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ789012";
+
+  // First, stop listening so we can talk.
+  radio.stopListening();
+
+  // Take the time, and send it.  This will block until complete
+  Serial.print(F("Now sending length "));
+  Serial.println(next_payload_size);
+  radio.write(send_payload, next_payload_size);
+
+  // Now, continue listening
+  radio.startListening();
+
+  // Wait here until we get a response, or timeout
+  unsigned long started_waiting_at = millis();
+  bool timeout = false;
+  while ( ! radio.available() && ! timeout )
+    if (millis() - started_waiting_at > 500 )
+      timeout = true;
+
+  // Describe the results
+  if ( timeout )
+  {
+    Serial.println(F("Failed, response timed out."));
+  }
+  else
+  {
+    // Grab the response, compare, and send to debugging spew
+    uint8_t len = radio.getDynamicPayloadSize();
+    
+    // If a corrupt dynamic payload is received, it will be flushed
+    if(!len){
+      return; 
+    }
+    
+    radio.read( receive_payload, len );
+
+    // Put a zero at the end for easy printing
+    receive_payload[len] = 0;
+
+    // Spew it
+    Serial.print(F("Got response size="));
+    Serial.print(len);
+    Serial.print(F(" value="));
+    Serial.println(receive_payload);
+  }
+  
+  // Update size for next time.
+  next_payload_size += payload_size_increments_by;
+  if ( next_payload_size > max_payload_size )
+    next_payload_size = min_payload_size;
+
+  // Try again 1s later
+  delay(100);
 }
