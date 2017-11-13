f76bcbdd0c5105315f134326a992e35642c88136
gregpaton08 <gregpaton08@gmail.com>
2017-11-13 16:13:51 -0500

    Remove unused code

--- switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
+++ switch_hardware/arduino/outletSwitchNRF24/outletSwitchNRF24.ino
@@ -63,7 +63,7 @@ const int max_payload_size = 32;
 const int payload_size_increments_by = 1;
 int next_payload_size = min_payload_size;
 
-char receive_payload[max_payload_size+1]; // +1 to allow room for a terminating NULL char
+char receive_payload[max_payload_size + 1]; // +1 to allow room for a terminating NULL char
 
 void setup(void)
 {
@@ -77,110 +77,17 @@ void setup(void)
   radio.enableDynamicPayloads();
 
   // optionally, increase the delay between retries & # of retries
-  radio.setRetries(5,15);
+  radio.setRetries(5, 15);
 
-  //
-  // Open pipes to other nodes for communication
-  //
-
-  // This simple sketch opens two pipes for these two nodes to communicate
-  // back and forth.
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-
-  if ( role == role_ping_out )
-  {
-    radio.openWritingPipe(pipes[0]);
-    radio.openReadingPipe(1,pipes[1]);
-  }
-  else
-  {
-    radio.openWritingPipe(pipes[0]);
-    radio.openReadingPipe(1,pipes[1]);
-  }
-
-  //
-  // Start listening
-  //
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1, pipes[1]);
 
   radio.startListening();
-
-  //
-  // Dump the configuration of the rf unit for debugging
-  //
-
   radio.printDetails();
 }
 
 void loop(void)
 {
-  //
-  // Ping out role.  Repeatedly send the current time
-  //
-
-  if (role == role_ping_out)
-  {
-    // The payload will always be the same, what will change is how much of it we send.
-    static char send_payload[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ789012";
-
-    // First, stop listening so we can talk.
-    radio.stopListening();
-
-    // Take the time, and send it.  This will block until complete
-    Serial.print(F("Now sending length "));
-    Serial.println(next_payload_size);
-    radio.write( send_payload, next_payload_size );
-
-    // Now, continue listening
-    radio.startListening();
-
-    // Wait here until we get a response, or timeout
-    unsigned long started_waiting_at = millis();
-    bool timeout = false;
-    while ( ! radio.available() && ! timeout )
-      if (millis() - started_waiting_at > 500 )
-        timeout = true;
-
-    // Describe the results
-    if ( timeout )
-    {
-      Serial.println(F("Failed, response timed out."));
-    }
-    else
-    {
-      // Grab the response, compare, and send to debugging spew
-      uint8_t len = radio.getDynamicPayloadSize();
-      
-      // If a corrupt dynamic payload is received, it will be flushed
-      if(!len){
-        return; 
-      }
-      
-      radio.read( receive_payload, len );
-
-      // Put a zero at the end for easy printing
-      receive_payload[len] = 0;
-
-      // Spew it
-      Serial.print(F("Got response size="));
-      Serial.print(len);
-      Serial.print(F(" value="));
-      Serial.println(receive_payload);
-    }
-    
-    // Update size for next time.
-    next_payload_size += payload_size_increments_by;
-    if ( next_payload_size > max_payload_size )
-      next_payload_size = min_payload_size;
-
-    // Try again 1s later
-    delay(100);
-  }
-
-  //
-  // Pong back role.  Receive each packet, dump it out, and send it back
-  //
-
   if ( role == role_pong_back )
   {
     // if there is data ready
