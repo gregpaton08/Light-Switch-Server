bacada6274048d4faaf19e67c1db80c394ea1893
gregpaton08 <gregpaton08@gmail.com>
2017-11-30 17:09:59 -0500

    Update script to create service

--- service/add_service.sh
+++ service/add_service.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-ln -s ../run.sh /etc/init.d/run_ls_server.sh
+script_link="/etc/init.d/run_ls_server.sh"
+if [ ! -f "$script_link" ]; then
+    echo "Adding symbolic like to /etc/init.d"
+    ln -s ../run.sh $script_link
+fi
+
+service="light_switch"
+if [ ! -e "/etc/init.d/$service" ]; then
+    echo "Adding service to etc/init.d"
+    cp $service /etc/init.d
+    update-rc.d sample.py defaults
+fi
