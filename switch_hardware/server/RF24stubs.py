8e61cf05c1a5ab8c1bb2e44be559677aefcc41f5
gregpaton08 <gregpaton08@gmail.com>
2017-12-04 17:55:44 -0500

    Update stubs to retain a fake switch status

--- switch_hardware/server/RF24stubs.py
+++ switch_hardware/server/RF24stubs.py
@@ -1,8 +1,21 @@
-
+import pickle
 
 class RF24:
     def __init__(self, cepin, cspin):
-        pass
+        self.status_file = '.rf24.stubs.status.pkl'
+
+    def __get_status(self):
+        try:
+            with open(self.status_file, 'rb') as file:
+                data = pickle.load(file)
+            return data['status']
+        except:
+            return False
+
+    def __set_status(self, status):
+        with open(self.status_file, 'wb') as file:
+            data = { 'status' : status }
+            pickle.dump(data, file)
 
     def begin(self):
         pass
@@ -32,7 +45,10 @@ class RF24:
         return True
 
     def read(self, len):
-        return "1"
+        return '1' if self.__get_status() else '0'
 
     def write(self, buf):
-        pass
+        if buf == bytes(1):
+            self.__set_status(True)
+        elif buf == bytes(0):
+            self.__set_status(False)
