b9f4f80dc468f1be4797088467b7e7b1c4fee697
gregpaton08 <gregpaton08@gmail.com>
2018-01-03 20:40:27 -0500

    Add test function to measure re response time

--- switch_hardware/server/outletswitch_tests.py
+++ switch_hardware/server/outletswitch_tests.py
@@ -2,6 +2,23 @@
 
 import outletswitch
 
+def measure_response_time(switch):
+    # Send the message.
+    switch.radio.stopListening()
+    switch.radio.write(bytes(1))
+    switch.radio.startListening()
+
+    # Wait here until for a response, or timeout.
+    started_waiting_at = switch.millis()
+    while not switch.radio.available():
+        if (switch.millis() - started_waiting_at) > 1000:
+            raise Exception('response timed out')
+
+    print('Response took {0} ms'.format(switch.millis() - started_waiting_at))
+
+    return switch.radio.read(switch.radio.getDynamicPayloadSize())
+
+
 switch = outletswitch.OutletSwitch()
 
 while 1:
@@ -15,5 +32,7 @@ while 1:
             switch.set_status(True)
         elif int(command) == 2:
             print(switch.get_status())
+        elif int(command) == 3:
+            print(measure_response_time(switch))
     except Exception as e:
         print(e)
