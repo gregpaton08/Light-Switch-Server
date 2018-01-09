151c5b6aae25da7212b824adb6a4c0d166bd19fc
gregpaton08 <gregpaton08@gmail.com>
2018-01-09 14:24:29 -0500

    Update alarm API

--- outletalarms/api.py
+++ outletalarms/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, abort, make_response
 from flask_restful import Resource, Api
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 
 app = Flask(__name__)
@@ -14,11 +15,24 @@ jobstores = {
 }
 scheduler = BackgroundScheduler(jobstores=jobstores)
 
-class OutletAlarmAPI(Resource):
+class OutletAlarm(Resource):
     def get(self, alarm_id):
         print('get for {0}'.format(alarm_id))
 
     def put(self, alarm_id):
         print('put for {0}'.format(alarm_id))
 
-api.add_resource(OutletAlarmAPI, _API_URL_ROUTE + 'alarm/<string:alarm_id>')
+    def delete(self, alarm_id):
+        print('delete for {0}'.format(alarm_id))
+
+
+class OutletAlarmList(Resource):
+    def get(self):
+        print('get OutletAlarmList')
+
+    def post(self):
+        print('post OutletAlarmList')
+
+
+api.add_resource(OutletAlarmList, '/alarms')
+api.add_resource(OutletAlarm, '/alarms/<alarm_id>')
