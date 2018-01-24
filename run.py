dd58c59729770edf45c1c8b8626ed4c8542c1b08
gregpaton08 <gregpaton08@gmail.com>
2018-01-23 21:16:30 -0500

    Handle termination signal

--- run.py
+++ run.py
@@ -2,8 +2,14 @@
 
 import sys
 from outletswitch import app
+import signal
 
 DEBUG = False
+
+def sigterm_handler(_signo, _stack_frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
 
 if __name__ == "__main__":
     port = 3333
@@ -15,6 +21,8 @@ if __name__ == "__main__":
     try:
         app.run(host='0.0.0.0', port=port, debug=DEBUG)
     except KeyboardInterrupt:
+        pass
+    finally:
         pass
 
     print 'light switch server shutting down...'
